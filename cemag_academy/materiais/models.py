9cf86baaece8ac14497ca00e97eaec919402bc3f
luan640 <luan640@gmail.com>
2024-05-29 10:18:18 -0300

    arquivos

--- cemag_academy/materiais/models.py
+++ cemag_academy/materiais/models.py
@@ -10,8 +10,10 @@ def validate_file_type(value):
         raise ValidationError('Tipo de arquivo não suportado.')
 
 class Pasta(models.Model):
-    nome = models.CharField(max_length=200, primary_key=True)
+    
+    nome = models.CharField(max_length=255, unique=True)  # Nome único, mas não chave primária
     descricao = models.TextField(null=True, blank=True)
+    
     created_by = models.ForeignKey(CustomUser, related_name='pasta_user', on_delete=models.CASCADE)
     created_at = models.DateTimeField(auto_now_add=True)
     modified_at = models.DateTimeField(auto_now=True)
@@ -20,12 +22,14 @@ class Pasta(models.Model):
         return self.nome
 
 class Material(models.Model):
-    pastas = models.ManyToManyField(Pasta, related_name='materiais')
+    
+    pasta = models.ForeignKey(Pasta, related_name='pasta_material', on_delete=models.CASCADE)
     nome = models.CharField(max_length=200)
     descricao = models.TextField(null=True, blank=True)
     video = models.FileField(upload_to='videos/', null=True, blank=True, validators=[validate_file_type])
     arquivo = models.FileField(upload_to='arquivos/', null=True, blank=True, validators=[validate_file_type])
     fotos = models.ImageField(upload_to='fotos/', null=True, blank=True, validators=[validate_file_type])
+    
     created_by = models.ForeignKey(CustomUser, related_name='material_user', on_delete=models.CASCADE)
     created_at = models.DateTimeField(auto_now_add=True)
     modified_at = models.DateTimeField(auto_now=True)
--- cemag_academy/materiais/views.py
+++ cemag_academy/materiais/views.py
@@ -1,3 +1,79 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect,get_object_or_404
+from django.contrib import messages
 
-# Create your views here.
+from .models import Pasta,Material
+from .forms import AddPasta,AddMaterial
+
+def pastas_add(request):
+    if request.method == 'POST':
+        form = AddPasta(request.POST)
+        if form.is_valid():
+            pasta = form.save(commit=False)
+            pasta.created_by = request.user
+            pasta.save()
+            return redirect('list-pasta')
+    else:
+        form = AddPasta()
+    return render(request, 'pastas/pasta_add.html', {
+        'form': form,
+    })
+
+def pastas_list(request):
+    pastas = Pasta.objects.all()
+    return render(request, 'pastas/pasta_list.html', {
+        'pastas': pastas
+    })
+
+def pastas_detail(request, pk):
+    pasta = get_object_or_404(Pasta, pk=pk)
+    return render(request, 'pastas/pasta_detail.html', {
+        'pasta': pasta
+    })
+
+def pasta_edit(request, pk):
+    pasta = get_object_or_404(Pasta, pk=pk)
+    if request.method == 'POST':
+        form = AddPasta(request.POST, instance=pasta)
+        if form.is_valid():
+            form.save()
+            messages.success(request, 'Pasta updated successfully.')
+            return redirect('list-pasta')
+    else:
+        form = AddPasta(instance=pasta)
+    return render(request, 'pastas/pasta_edit.html', {
+        'form': form,
+    })
+
+def pasta_delete(request, pk):
+    pasta = get_object_or_404(Pasta, pk=pk)
+    pasta.delete()
+
+    messages.success(request, 'Pasta excluída com sucesso.')
+
+    return redirect('list-pasta')
+
+def material_add(request):
+    if request.method == 'POST':
+        form = AddMaterial(request.POST)
+        if form.is_valid():
+            material = form.save(commit=False)
+            material.created_by = request.user
+            material.save()
+            return redirect('list-material')
+    else:
+        form = AddMaterial()
+    return render(request, 'materiais/material_add.html', {
+        'form': form,
+    })
+
+def material_list(request):
+    materiais = Material.objects.all()
+    return render(request, 'materiais/material_list.html', {
+        'materiais': materiais
+    })
+
+def material_detail(request, pk):
+    material = get_object_or_404(Material, pk=pk)
+    return render(request, 'materiais/material_detail.html', {
+        'material': material
+    })
