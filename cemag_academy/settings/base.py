f4e7d5492d57dce1c6c0e54cce50f060385c08ef
luan <luan640@gmail.com>
2025-11-24 17:07:01 -0300

    adicionando storages.backends.s3boto3.S3Boto3Storage

--- cemag_academy/settings/base.py
+++ cemag_academy/settings/base.py
@@ -93,7 +93,7 @@ AWS_S3_CUSTOM_DOMAIN = f'{AWS_STORAGE_BUCKET_NAME}.s3.amazonaws.com'
 
 STORAGES = {
     "default": {
-        "BACKEND": DEFAULT_FILE_STORAGE,
+        "BACKEND": "storages.backends.s3boto3.S3Boto3Storage",
     },
     "staticfiles": {
         "BACKEND": "whitenoise.storage.CompressedManifestStaticFilesStorage",
